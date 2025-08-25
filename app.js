7da6a5bd9bf5f73cd65cbe270a5688187185b2c5
Ginatam32 <fluffey6@gmail.com>
2025-08-25 16:06:58 -0500

    cors fix try

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ app.use(cors({
     'https://imageanalyzerbackend-production.up.railway.app'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 }));
 app.use(express.json());
 import routers from "./routes/routes.js";
