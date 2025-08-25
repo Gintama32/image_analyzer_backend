699078d9da1727ea8ce5649299491a0763321272
Ginatam32 <fluffey6@gmail.com>
2025-08-25 16:02:08 -0500

    addded cors

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://fastidious-wisp-9a1497.netlify.app'],
+  origin: [
+    'http://localhost:5173', 
+    'https://fastidious-wisp-9a1497.netlify.app',
+    'https://imageanalyzerbackend-production.up.railway.app'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
